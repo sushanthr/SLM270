7f815e2bc8e4ada1f9cc5ecabd1b86c26d5444fd
Sushanth Rajasankar <me.sushanth@gmail.com>
2026-03-28 11:13:30 -0700

    Update batch size to 256

--- train.py
+++ train.py
@@ -36,8 +36,8 @@ class TrainConfig:
     seq_len: int        = 1024
 
     # Batch
-    batch_size: int     = 32
-    grad_accum: int     = 2                 # effective batch = 32 × 2 × 1024 = 65 536 tok
+    batch_size: int     = 256
+    grad_accum: int     = 1                 # effective batch = 256 × 1 × 1024 = 262 144 tok
 
     # Optimiser
     max_lr: float       = 3e-4
