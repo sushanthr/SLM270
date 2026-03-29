9c0ccb2db1965316580f9b5254221a67ab9ce217
Sushanth Rajasankar <me.sushanth@gmail.com>
2026-03-29 16:56:06 -0700

    Add licensing and source information to SLM270.py
    Added licensing information and source attribution for Gemma3 implementation.

--- SLM270.py
+++ SLM270.py
@@ -1,3 +1,22 @@
+"""
+Gemma3 Implementation is sourced from 
+https://github.com/rasbt/LLMs-from-scratch/blob/6b9502056fba9388cdbb93c8a7d2d0d9d5deec81/ch05/12_gemma3/standalone-gemma3.ipynb
+
+Copyright 2023-2026 Sebastian Raschka
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
 import os
 from transformers import PreTrainedTokenizerFast
 import torch
